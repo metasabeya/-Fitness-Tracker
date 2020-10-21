85b1874a1d08e10ba17df57dd01f4e2cea92dac1
unknown <metybuza46@gmail.com>
2020-10-21 11:49:50 -0600

    added workout.js

---
+++ server.js
@@ -0,0 +1,34 @@
+//install packages
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+//set port
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//use logger
+app.use(logger("dev"));
+
+//parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//use static files
+app.use(express.static("public"));
+
+const URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+
+mongoose.connect(URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
