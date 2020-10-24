5c1b6a7f40f05e8fa598300e08365c08ec354455
unknown <metybuza46@gmail.com>
2020-10-23 19:42:22 -0600

    final commit

--- seeders/seed.js
+++ seeders/seed.js
@@ -131,4 +131,7 @@ db.Workout.deleteMany({})
     console.log(data.result.n + " records inserted!");
     process.exit(0);
   })
-  
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
